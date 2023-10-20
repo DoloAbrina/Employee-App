1ab81d053d8aeb642399da5c6aaf372dcedd2cb4
Dolo Abrina <dolosejaabrina@gmail.com>
2023-10-20 15:07:34 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Login from './Components/Login';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Login/>
+      <h1>Hello World!</h1>
+    </div>
+  );
+}
+
+export default App;
